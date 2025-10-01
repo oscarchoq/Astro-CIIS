735d088d8439ccc523a7e64d33d4830221ffbb28
Oscar Choque <89710118+oscarchoq@users.noreply.github.com>
2025-10-01 00:35:27 -0500

    Update Schedules.tsx

--- src/views/schedules/Schedules.tsx
+++ src/views/schedules/Schedules.tsx
@@ -44,7 +44,7 @@ const Schedules = () => {
       id="schedules"
       className="w-full flex flex-col items-center mb-24 max-w-screen-md mx-auto pt-40 sm:pt-48"
     >
-      <h2 className="text-4xl font-bold text-center text-secondary sm:text-6xl slide-top">
+      <h2 className="text-4xl font-bold text-center text-secondary sm:text-6xl slide-top leading-relaxed sm:leading-loose">
         Cronograma
       </h2>
       <p className="text-center text-base mb-8 mt-8">
